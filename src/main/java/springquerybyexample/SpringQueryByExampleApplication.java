4bdd83ef613a55b845d4fb3b1f2b733d73bbcfe0
Boilerr <sergey.nys@gmail.com>
2023-12-06 20:08:25 +0200

    Add Spring Boot 3.2.0 template from start.spring.io

---
+++ src/main/java/springquerybyexample/SpringQueryByExampleApplication.java
@@ -0,0 +1,13 @@
+package springquerybyexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringQueryByExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringQueryByExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/springquerybyexample/SpringQueryByExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package springquerybyexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringQueryByExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
